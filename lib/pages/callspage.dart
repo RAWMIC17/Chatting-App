725d4168a44537484c866ec99d8b4511077f8a50
RAWMIC17 <hks1710harsh@gmail.com>
2024-04-28 23:56:27 +0530

    a

--- lib/pages/callspage.dart
+++ lib/pages/callspage.dart
@@ -35,7 +35,7 @@ class _CallsPageState extends State<CallsPage> {
         //     child: Icon(
         //       Icons.notifications_active_rounded,
         //       color: Vx.white,
-        //       size: 30,
+        //       size: 30,k
         //     ),
         //   ),
         // ),
